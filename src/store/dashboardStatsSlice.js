fa739dd1d4e389352acdad8d0f92b248805707ba
aaditkamat <plunges_abiotic.0q@icloud.com>
2025-02-21 03:58:35 -0500

    Manage app states depending on fetch data using Redux

---
+++ src/store/dashboardStatsSlice.js
@@ -0,0 +1,59 @@
+import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
+import sendApiRequest from '../../util/apiRequest.js';
+import _ from "lodash";
+
+export const fetchProjectSavingsDataFromApi = createAsyncThunk(
+    'fetchProjectSavingsDataFromApi', async () => {
+        try {
+            let query = "{\n" +
+                "  boards(ids: [6589717059]) {\n" +
+                "    items_page(limit: 500) {\n" +
+                "      items {\n" +
+                "        column_values(\n" +
+                "          ids: [\"numbers3__1\", \"numbers4__1\", \"numbers_2__1\", \"numbers_3__1\", \"numbers_4__1\", \"numbers_5__1\", \"numbers05__1\"]\n" +
+                "        ) {\n" +
+                "          column {\n" +
+                "            title\n" +
+                "          }\n" +
+                "          text\n" +
+                "        }\n" +
+                "      }\n" +
+                "    }\n" +
+                "  }\n" +
+                "}";
+            const responseData = await sendApiRequest(query);
+            return responseData?.data?.boards[0].items_page?.items;
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            throw error;
+        }
+    });
+
+const dashboardStatsSlice = createSlice({
+    name: 'dashboardStats',
+    initialState: {},
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(fetchProjectSavingsDataFromApi.fulfilled, (state, action) => {
+            let newData = {};
+            _.forEach(action.payload, (item) => {
+                const column_values = item?.column_values;
+                _.forEach(column_values, (column_value) => {
+                    const key = column_value?.column?.title;
+                    let value = parseFloat(column_value?.text);
+                    if (_.isNaN(value)) {
+                        value = 0;
+                    }
+                    if (_.includes(_.keys(newData), key)) {
+                        newData[key].push(value);
+                    } else {
+                        newData[key] = [value];
+                    }
+                });
+            });
+            return newData;
+        });
+    }
+});
+
+export default dashboardStatsSlice.reducer;
---
+++ src/store/topographySlice.js
@@ -0,0 +1,36 @@
+import {createAsyncThunk, createSlice} from '@reduxjs/toolkit';
+import * as d3 from "d3";
+import _ from "lodash";
+import {setCountyCounts} from "./countyCountsSlice.js";
+
+export const fetchGeoJsonDataLocally = createAsyncThunk(
+    'fetchGeoJsonDataLocally', async () => {
+    try {
+        return d3.json('/data/va_county.geojson');
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        throw error;
+    }
+});
+
+const topographySlice = createSlice({
+    name: 'topography',
+    initialState: null,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(fetchGeoJsonDataLocally.fulfilled, (state, action) => {
+            return action.payload;
+        });
+
+        builder.addCase(setCountyCounts, (state, action) => {
+            const updatedGeoJson = state;
+            updatedGeoJson.features.forEach((feature) => {
+                const county = feature.properties.NAMELSAD.trim();
+                feature.properties.count = action.payload[county] || 0;
+            });
+            return updatedGeoJson;
+        });
+    }
+});
+
+export default topographySlice.reducer;
