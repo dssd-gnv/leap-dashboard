db6f8412caf91e062c9a21f0a98fcc52051d838b
aaditkamat <plunges_abiotic.0q@icloud.com>
2025-02-22 13:11:56 -0500

    Modify structure of data to allow mapping from column names to dashboard label and display of data in a particular order

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,22 +1,31 @@
-import Map from "./Map";
 import { Fragment } from "react";
 import _ from "lodash";
 import DashboardMap from "./DashboardMap.jsx";
 
-function createStatCard(showHouseHoldAverages, currencyColumns, column, values) {
+function createMap(dashboardStats, columnMapping) {
+    const dashboardStatsMap = new Map();
+    _.chain(_.map(columnMapping, (entry) => entry.column)).forEach((column) => {
+        dashboardStatsMap.set(column, dashboardStats[column]);
+    }).value();
+    return dashboardStatsMap;
+}
+
+function createStatCard(showHouseHoldAverages, columnMapping, column, values) {
     let aggregatedValue;
     if (!showHouseHoldAverages) {
         aggregatedValue = _.round(_.sum(values) / 1000) + 'K';
     } else {
         aggregatedValue = _.round(_.mean(values));
     }
-    if (_.includes(currencyColumns, column)) {
+    const isCurrencyType = _.chain(columnMapping).filter(entry => entry.type === 'currency').map((entry) => entry.column).includes(column).value();
+    if (isCurrencyType) {
         aggregatedValue = '$' + aggregatedValue;
     }
+    const dashboardDisplay = _.chain(columnMapping).filter(entry => entry.column === column).map((entry) => entry.dashboardDisplay).value();
     return (
         <Fragment key={column}>
             <div className="card stat-card">
-                <h2 className="stat-heading">{column}</h2>
+                <h2 className="stat-heading">{dashboardDisplay}</h2>
                 <span className="stat">{aggregatedValue}</span>
             </div>
         </Fragment>
@@ -25,22 +34,46 @@ function createStatCard(showHouseHoldAverages, currencyColumns, column, values) 
 }
 
 export default function Dashboard({ showHouseholdAverages, dashboardStats, topography, countyCounts }) {
-    const numericalColumns = [
-        'kWh Saved',
-        'CO₂ Tons',
-        'Annual Fuel Therms Saved'
+    const columnMapping = [
+        {
+            'column': 'kWh Saved',
+            'dashboardDisplay': 'kWh Saved',
+            'type': 'numerical',
+        },
+        {
+            'column': 'Annual Fuel Therms Saved',
+            'dashboardDisplay': 'Annual Fuel Therms Saved',
+            'type': 'numerical',
+        },
+        {
+            'column': 'Annual Fuel Dollars',
+            'dashboardDisplay': 'Total Annual Energy Savings',
+            'type': 'currency',
+        },
+        {
+            'column': 'Annual Electric Dollars',
+            'dashboardDisplay': 'Annual Electric Dollars',
+            'type': 'currency',
+        },
+        {
+            'column': 'CO₂ Tons',
+            'dashboardDisplay': 'CO₂ Tons',
+            'type': 'numerical',
+        },
+        {
+            'column': 'Total Savings',
+            'dashboardDisplay': 'Cumulative Savings',
+            'type': 'currency',
+        },
     ];
-    const currencyColumns = [
-        'Annual Fuel Dollars',
-        'Annual Electric Dollars',
-        'Total Savings'
-    ];
-    let firstRowColumns = _.concat(numericalColumns, [currencyColumns[0]])
-    let secondRowColumns = _.range(1, currencyColumns.length).map((idx) => currencyColumns[idx]);
+
+    const dashboardStatsMap = createMap(dashboardStats, columnMapping);
+    const firstRowColumns = _.chain(columnMapping).map((entry) => entry.column).take(4).value();
+    const secondRowColumns = _.chain(columnMapping).map((entry) => entry.column).splice(4).value();
     return (
         <div className="grid grid-cols-4 gap-4">
             {
-                Object.entries(dashboardStats)
+                Array.from(dashboardStatsMap.entries())
                     .filter((entry) => {
                         let column = entry[0];
                         return _.includes(firstRowColumns, column);
@@ -48,7 +81,7 @@ export default function Dashboard({ showHouseholdAverages, dashboardStats, topog
                     .map((entry) => {
                         let column = entry[0];
                         let values = entry[1];
-                        return createStatCard(showHouseholdAverages, currencyColumns, column, values);
+                        return createStatCard(showHouseholdAverages, columnMapping, column, values);
                     })
             }
             <div className="map-container col-span-3 row-span-3">
@@ -59,7 +92,7 @@ export default function Dashboard({ showHouseholdAverages, dashboardStats, topog
                 />
             </div>
             {
-                Object.entries(dashboardStats)
+                Array.from(dashboardStatsMap.entries())
                     .filter((entry) => {
                         let column = entry[0];
                         return _.includes(secondRowColumns, column);
@@ -67,7 +100,7 @@ export default function Dashboard({ showHouseholdAverages, dashboardStats, topog
                     .map((entry) => {
                         let column = entry[0];
                         let values = entry[1];
-                        return createStatCard(showHouseholdAverages, currencyColumns, column, values);
+                        return createStatCard(showHouseholdAverages, columnMapping, column, values);
                     })
             }
         </div >
