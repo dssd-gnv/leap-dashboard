390743db196794babbe2ea8be9046689367b1443
aaditkamat <plunges_abiotic.0q@icloud.com>
2025-03-12 12:45:41 -0400

    Add drawer and menu button for smaller screen sizes

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,19 @@ import { fetchGeoJsonDataLocally } from "./store/topographySlice.js";
 import { fetchIntakeDataFromApi } from "./store/countyCountsSlice.js";
 import { fetchProjectSavingsDataFromApi } from "./store/dashboardStatsSlice.js";
 import { fetchSolarInstallsDataFromApi } from "./store/solarInstallsDataSlice.js";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import Drawer from 'react-modern-drawer';
+import 'react-modern-drawer/dist/index.css';
 
 function App() {
     const { topographyLoading, topographyData } = useSelector(state => state.topography);
     const { countyCountsLoading, countyCountsData } = useSelector(state => state.countyCounts);
     const { dashboardStatsLoading, dashboardStatsData} = useSelector(state => state.dashboardStats);
     const { solarInstallsDataLoading, solarInstallsData } = useSelector(state => state.solarInstallsData);
+    const [menuOpen, setMenuOpen] = useState(false);
     const dispatch = useDispatch();
     const [showHouseholdAverages, setShowHouseholdAverages] = useState(false);
+
     const displayToggle = (window) => {
         if (window.location.pathname === "/") {
             return (
@@ -75,7 +80,7 @@ function App() {
     return (
         <Router>
             <div className='max-w-screen overflow-x-hidden'>
-                <div className='fixed inset-x-0 top-0 z-10 border-b border-black/5 dark:border-white/10'>
+                <nav className='fixed inset-x-0 top-0 z-10 border-b border-black/5 dark:border-white/10'>
                     <div className='bg-white'>
                         {/* Main navbar content */}
                         <div className='flex h-14 items-center justify-between gap-8 px-4 sm:px-6'>
@@ -86,22 +91,42 @@ function App() {
                                     width={100}
                                 />
                             </Link>
-                            <div className='flex justify-end items-center gap-[1vw]'>
+                            <div className='hidden md:flex justify-end items-center gap-[1vw]'>
                                 {
                                     displayToggle(window)
                                 }
-                                <div className='hidden md:flex items-center gap-[1vw]'>
-                                    <Link to="https://www.leap-va.org/"
-                                          className="py-[10px] px-[20px] bg-[#386fa4] font-bold text-white rounded-md hover:bg-opacity-90 transition-all duration-200 ease-out">About
-                                        LEAP</Link>
-                                    <Link to="/About"
-                                          className="py-[10px] px-[20px] bg-[#386fa4] font-bold text-white rounded-md hover:bg-opacity-90 transition-all duration-200 ease-out">About
-                                        this Dashboard</Link>
-                                </div>
+                                <Link to="https://www.leap-va.org/"
+                                      className="py-2 px-2 bg-[#386fa4] font-bold text-white text-nowrap rounded-md hover:bg-opacity-90 transition-all duration-200 ease-out">About
+                                    LEAP</Link>
+                                <Link to="/About"
+                                      className="py-2 px-2 bg-[#386fa4] font-bold text-white text-nowrap rounded-md hover:bg-opacity-90 transition-all duration-200 ease-out">About
+                                    this Dashboard</Link>
+                            </div>
+                            <div className='md:hidden'>
+                                <Bars3Icon className='h-6 w-6 cursor-pointer' onClick={() => setMenuOpen(!menuOpen)}/>
+                                {
+                                    menuOpen && (
+                                        <div className='fixed inset-0 top-16 z-5'>
+                                            <Drawer
+                                                open={menuOpen}
+                                                direction='right'
+                                            >
+                                                <XMarkIcon className='fixed top-0 right-0 h-6 w-6 cursor-pointer' onClick={() => setMenuOpen(false)}/>
+                                                <Link to="https://www.leap-va.org/" className="block px-4 py-3 rounded-lg font-bold text-black dark:text-white">
+                                                    About LEAP
+                                                </Link>
+                                                <br />
+                                                <Link to="/About" className="block px-4 py-3 rounded-lg font-bold text-black dark:text-white">
+                                                    About this Dashboard
+                                                </Link>
+                                            </Drawer>
+                                        </div>
+                                    )
+                                }
                             </div>
                         </div>
                     </div>
-                </div>
+                </nav>
                 <Fragment>
                     <Routes>
                         <Route path="/About" element={<About/>}/>
